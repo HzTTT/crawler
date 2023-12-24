90cb09fa4704ded32467fa2acaca00d9701f4e2f
HzTTT <767067749@qq.com>
2023-12-24 15:46:57 +0800

    feat(main):add proxy fetch google

--- main.go
+++ main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
+	"github.com/HzTTT/crawler/collect"
+	"github.com/HzTTT/crawler/proxy"
+	"time"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	resq, err := http.Get(url)
+	proxyURLs := []string{"http://127.0.0.1:7890"}
+	proxyFunc, err := proxy.RoundRobinProxyFunc(proxyURLs...)
+	if err != nil {
+		return
+	}
+	url := "https://www.google.com/"
+	fetcher := collect.BrowserFetch{
+		Timeout: 3000 * time.Millisecond,
+		Proxy:   proxyFunc,
+	}
+	body, err := fetcher.Get(url)
 	if err != nil {
 		fmt.Printf("fetch url error : %s", err)
 		return
 	}
-
-	body, err := io.ReadAll(resq.Body)
-	if err != nil {
-		fmt.Printf("read body error : %s", err)
-		return
-	}
-
-	fmt.Printf("%s", body)
+	fmt.Printf("body : %s", body)
 }
