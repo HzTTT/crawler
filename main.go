fdd6899357808a40c0ba300c560936dac2953802
HzTTT <767067749@qq.com>
2023-12-25 13:45:47 +0800

    feat(main):deploy log

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/HzTTT/crawler/collect"
+	"github.com/HzTTT/crawler/log"
 	"github.com/HzTTT/crawler/proxy"
+	"go.uber.org/zap"
+
 	"time"
 )
 
 func main() {
+	plugin, closer := log.NewStdFilePlugin("./log.txt", zap.InfoLevel)
+	defer closer.Close()
+	logger := log.NewLogger(plugin)
+	logger.Info("log init end")
 	proxyURLs := []string{"http://127.0.0.1:7890"}
 	proxyFunc, err := proxy.RoundRobinProxyFunc(proxyURLs...)
 	if err != nil {
